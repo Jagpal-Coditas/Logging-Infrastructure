0df6266cc3144d586c8081f2dc6bd374efe3ef61
Jagpal-Coditas <Jagpal.sisodiya@coditas.com>
2021-06-16 18:32:06 +0530

    Added test project

---
+++ TestWithDotnetFramework/CurrentContextService.cs
@@ -0,0 +1,13 @@
+﻿using Logging.Common;
+using System.Web.Mvc;
+
+namespace TestWithDotnetFramework
+{
+    public class CurrentContextService : ICurrentContextService
+    {
+        public ILoggerContext GetLoggerContext()
+        {
+            return DependencyResolver.Current.GetService<ILoggerContext>();
+        }
+    }
+}
---
+++ TestWithDotnetFramework/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestWithDotnetFramework.MvcApplication" Language="C#" %>
